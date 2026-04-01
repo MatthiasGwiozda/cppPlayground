733b5cd16b026f7f5494e65920bddac68f868e46
MatthiasGwiozda <matzeqwertz@gmail.com>
2026-04-01 19:16:33 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main2()
+{
+    char a[] = "asdf223332";
+    int b[] = {1, 2};
+    std::cout << a;
+    return 0;
+}
+
+int main()
+{
+    main2();
+    // int i = 1;
+    // int b = ++i;
+    // cout << b;
+}
